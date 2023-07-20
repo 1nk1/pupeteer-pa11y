b13c4432bd5ad534cd40468c5c9feb2dab4a2b11
Andrew <Peretyatko>
2023-07-20 15:40:37 +0300

    Fix ['Report']

--- src/core/config/pally.config.js
+++ src/core/config/pally.config.js
@@ -1,31 +1,32 @@
-const getDate = require('../../core/helper/utils');
+import { getDate } from '../../core/helper/utils';
 
-const options = {
-	runners: [
-		'axe',
-		'htmlcs'
-	],
-	standard: ['WCAG2A', 'WCAG2AA', 'WCAG2AAA'],
-	timeout: 30000,
-	threshold: 0,
-	viewport: {
-		width: 1920,
-		height: 1080,
-		deviceScaleFactor: 2,
-		isMobile: false
-	},
-	includeNotices: true,
-	includeWarnings: true,
-	log: {
-		debug: console.log,
-		error: console.error,
-		info: console.info
-	},
-	screenCapture: `../screen/${getDate}.png`,
-	reporter: "html-plus",
-	reporterOptions: {
-		"outputFile": "./reports/pa11y-report.html"
-	}
+export default async function options() {
+	const date = await getDate();
+	return {
+		runners: [
+			'axe',
+			'htmlcs'
+		],
+		standard: ['WCAG2A', 'WCAG2AA', 'WCAG2AAA'],
+		timeout: 30000,
+		threshold: 0,
+		viewport: {
+			width: 1920,
+			height: 1080,
+			deviceScaleFactor: 2,
+			isMobile: false
+		},
+		includeNotices: true,
+		includeWarnings: true,
+		log: {
+			debug: console.log,
+			error: console.error,
+			info: console.info
+		},
+		screenCapture: `../screen/${date}.png`,
+		reporter: "html-plus",
+		reporterOptions: {
+			"outputFile": "./reports/pa11y-report.html"
+		}
+	};
 }
-
-module.exports = options;
